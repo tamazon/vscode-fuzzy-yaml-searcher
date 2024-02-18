a1dcce2886dfe274a0851501ab11d96db3b5598b
tamazon <35480652+tamazon@users.noreply.github.com>
2024-02-18 20:57:31 +0900

    js-yamlでyaml読む

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { load } from 'js-yaml';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,6 +10,15 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "fuzzy-yaml-sercher" is now active!');
+
+	let textEditor = vscode.window.activeTextEditor;
+	if (textEditor) {
+		const document = textEditor.document;
+		const test_string: string = document.getText();
+		console.log(test_string);
+		const yaml_data = load(test_string);
+		console.log(yaml_data);
+	}
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
